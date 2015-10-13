1d14ec6efea07f31c2459a92e22464e289e757f6
Anna <anya.rezistor@gmail.com>
2015-10-13 20:40:34 +0300

    Define of VK-user's age

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from flask import render_template, flash, request
+from app import app
+from app import find_age
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+	results = {}
+	errors = []
+	if request.method == 'POST':
+		try:
+			user_id = request.form['id']
+			
+			if user_id:
+				age = find_age.find_user_age(int(user_id))
+				results  = [('Method 1', age[0]), 
+							('Method 2', age[1]), 
+							('Method 3', age[2]), 
+							('Method 4', age[3]),
+							('Method 5', age[4])]
+				return render_template('index.html', errors = errors, user_id = user_id, results = results)
+			else:
+				errors.append("The form should not be empty")
+				return render_template('index.html', errors = errors)
+		except:
+			errors.append("Wrong id or no access the Internet. Please make sure it is right and try again")
+			return render_template('index.html', errors = errors)
+				
+	return render_template('index.html')
---
+++ flask/Scripts/easy_install-script.py
@@ -0,0 +1,9 @@
+#!"C:\Program Files (x86)\microblog-0.1\flask\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==18.3.2','console_scripts','easy_install'
+__requires__ = 'setuptools==18.3.2'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('setuptools==18.3.2', 'console_scripts', 'easy_install')()
+)
